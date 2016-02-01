21beb8281f379045fa3012bc8905be8322c843a3
sasankakella <sasank.akella@hotmail.com>
2016-02-01 12:57:29 +0530

    commites

---
+++ MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
